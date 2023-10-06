40f9bc5a7af9bae364039b96a476e19be51b6e7b
zhou <zhou@axe.bz>
2023-10-06 18:22:06 +0900

    Modify testbench

--- AdderHF1bit/AdderHF1bit_tb.v
+++ AdderHF1bit/AdderHF1bit_tb.v
@@ -3,11 +3,32 @@
 module AdderHF1bit_tb (
     // None
 );
+
+    reg  in1, in2; 
+    wire out;
     initial begin
-        $display("Hello World!");
+           in1 = 1'b0;
+           in2 = 1'b0;
+        #5 in1 = 1'b0;
+        #5 in2 = 1'b1;
+        #5 in1 = 1'b1;
+        #5 in2 = 1'b0;
+        #5 in1 = 1'b1;
+        #5 in2 = 1'b1;
+    end
+
+    AdderHF1bit adder(
+        .in1(in1), .in2(in2),
+        .out(out)
+    );
+
+    initial begin
+        $display("Finish");
+        $dumpfile("AdderHF1bit.vcd");
+        $dumpvars(0, in1);
+        $dumpvars(1, in2);
+        $dumpvars(2, out);
         $finish;
     end
-    
-    // AdderHF1bit adder();
 
 endmodule
