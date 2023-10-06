a79ff81b80a459f54999517e59805fc3aedcab21
zhou <zhou@axe.bz>
2023-10-06 17:35:31 +0900

    Prepare for RTL testbench

--- AdderHF1bit/AdderHF1bit_tb.v
+++ AdderHF1bit/AdderHF1bit_tb.v
@@ -1,1 +1,13 @@
+`include "AdderHF1bit.v"
 
+module AdderHF1bit_tb (
+    // None
+);
+    initial begin
+        $display("Hello World!");
+        $finish;
+    end
+    
+    // AdderHF1bit adder();
+
+endmodule
