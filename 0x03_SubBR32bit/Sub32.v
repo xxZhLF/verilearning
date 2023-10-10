49c234472309b9c1a7ae0307188253fa3188d0f5
zhou <zhou@axe.bz>
2023-10-10 12:50:23 +0900

    Add wrapper

---
+++ 0x03_SubBR32bit/Sub32.v
@@ -0,0 +1,20 @@
+`include "SubBR32bit.v"
+
+module Sub32(
+    input  wire [31:0] op1,
+    input  wire [31:0] op2,
+    output wire [31:0] diff
+);
+
+    reg ZERO = 1'b0;
+    reg NULL;
+
+    SubBR32bit subtractor(
+        .op1(op1),
+        .op2(op2),
+        .bo(ZERO),
+        .diff(diff),
+        .bi(NULL)
+    );
+
+endmodule
