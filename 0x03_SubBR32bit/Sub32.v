8809a51ae914e05f9fb768b2fc04118d854a8a28
zhou <zhou@axe.bz>
2023-10-10 16:32:29 +0900

    Remove borrow bit function

--- 0x03_SubBR32bit/Sub32.v
+++ 0x03_SubBR32bit/Sub32.v
@@ -1,18 +1,80 @@
+// `define SUB_DEBUG_ON
+
 module Sub32(
     input  wire [31:0] op1,
     input  wire [31:0] op2,
     output wire [31:0] diff
+`ifdef SUB_DEBUG_ON
+  , output wire [31:0] debug
+`endif
 );
 
-    reg ZERO = 1'b0;
-    reg NULL;
-
-    SubBR32bit subtractor(
-        .op1(op1),
-        .op2(op2),
-        .bo(ZERO),
-        .diff(diff),
-        .bi(NULL)
+    wire [31:0] op1C, op2C;
+    ModT2C modT2C_a(  /* Complement of op1 */ 
+        .T(op1),
+        .C(op1C)
+    ), modT2C_b(  /* Complement of -op2 */ 
+        .T({~op2[31], op2[30:0]}),
+        .C(op2C)
     );
+
+    wire [31:0] diffC;
+    Add32 adder(  
+        .op1(op1C), 
+        .op2(op2C), 
+        .sum(diffC)
+    );
+
+    ModC2T modC2T(
+        .C(diffC),
+        .T(diff)
+    );  /* Truth of result */ 
+
+`ifdef SUB_DEBUG_ON
+    assign debug = op1C;
+`endif
+
+endmodule
+
+module ModT2C(
+    input  wire [31:0] T,
+    output wire [31:0] C
+);
+
+    wire        sign; 
+    wire [30:0] datI;
+    assign {sign, datI} = T;
+
+    wire [31:0] datO;
+
+    Add32 adder(
+        .op1({1'b0, ~datI}), 
+        .op2(32'h00000001), 
+        .sum(datO)
+    );  /* ~datI + 1 */
+
+    assign C = datI != {28'h0000000, 3'b000} ? {sign, sign ? datO[30:0] : datI[30:0]} : 32'h00000000;
+
+endmodule
+
+module ModC2T(
+    input  wire [31:0] C,
+    output wire [31:0] T
+);
+
+    wire        sign; 
+    wire [30:0] datI;
+    assign {sign, datI} = C;
+
+    wire [31:0] datO;
+
+    Add32 adder(
+        .op1({1'b0, datI}), 
+        .op2(32'hFFFFFFFF), 
+        .sum(datO)
+    );  /* ~(datI + -1) 
+           ~ at next line */
+
+    assign T = datI != {28'h0000000, 3'b000} ? {sign, sign ? ~datO[30:0] : datI[30:0]} : 32'h00000000;
 
 endmodule
---
+++ 0x03_SubBR32bit/Sub32_tb.v
@@ -0,0 +1,46 @@
+`timescale 1ps/1ps
+
+// `define SUB_DEBUG_ON
+
+module Sub32_tb (
+    // None
+);
+`ifdef SUB_DEBUG_ON
+    wire [31:0] debug;
+`endif 
+
+    reg  [31:0] op1, op2;
+    wire [31:0] diff;
+    initial begin
+        op1 = 32'h00000000;
+        op2 = 32'h00000000;
+    end
+
+    Sub32 subtractor(
+        .op1(op1),
+        .op2(op2),
+        .diff(diff)
+`ifdef SUB_DEBUG_ON
+      , .debug(debug)
+`endif     
+    );
+
+    initial begin
+        #5 op1 = 32'h00000004; op2 = 32'h00000007;  /*  4 - 7 */
+        #5 op1 = 32'h80000004; op2 = 32'h00000007;  /* -4 - 7 */
+        #5 op1 = 32'h00000007; op2 = 32'h00000004;  /*  7 - 4 */
+        #5 op1 = 32'h80000007; op2 = 32'h00000004;  /* -7 - 4 */
+        #5 ;
+    end
+
+    initial begin
+        $dumpfile("Sub32.vcd");
+        $dumpvars(0, op1);
+        $dumpvars(1, op2);
+        $dumpvars(2, diff);
+`ifdef SUB_DEBUG_ON
+        $dumpvars(3, debug);
+`endif 
+    end
+
+endmodule
