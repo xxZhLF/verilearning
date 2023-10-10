ef70db5d727cfa65f4e5496aff131bc03b85be70
Zhou Xiaoxiao <xxzhou_LF94@outlook.com>
2023-10-11 00:00:26 +0900

    Add shortcut for CLI

--- rename.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-mv 0x00_AdderHF1bit     0x0000_AdderHF1bit
-mv 0x01_AdderFL1bit     0x0001_AdderFL1bit
-mv 0x02_AdderCH32bit    0x0002_AdderCH32bit
-mv 0x03_AdderLA32bit    0x0003_AdderLA32bit
-mv 0x04_Add32           0x0004_Add32
-mv 0x05_Sub32           0x0010_Sub32
-mv 0x06_Mul32           0x0020_Mul32
---
+++ shortcut.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# xxzh: 
+# Commands that are complicated to edit on the CLI 
+# are executed using this shell script.
+
+echo "RUNNING ----------------------------"
+
+echo "------------------------------- DONE"
