f2a60e7ca3f28caa3bd8ffa6ede233a7f47aaec2
Zhou Xiaoxiao <xxzhou_LF94@outlook.com>
2023-10-11 20:01:28 +0900

    Independent converter module

--- 0x0010_Sub32/Sub32.v
+++ 0x0010_Sub32/Sub32.v
@@ -1,5 +1,7 @@
 // `define SUB_DEBUG_ON
 
+`include "TC_converter.v"
+
 module Sub32(
     input  wire [31:0] op1,
     input  wire [31:0] op2,
@@ -33,48 +35,5 @@ module Sub32(
 `ifdef SUB_DEBUG_ON
     assign debug = op1C;
 `endif
-
-endmodule
-
-module ModT2C(
-    input  wire [31:0] T,
-    output wire [31:0] C
-);
-
-    wire        sign; 
-    wire [30:0] datI;
-    assign {sign, datI} = T;
-
-    wire [31:0] datO;
-
-    Add32 adder(
-        .op1({1'b0, ~datI}), 
-        .op2(32'h00000001), 
-        .sum(datO)
-    );  /* ~datI + 1 */
-
-    assign C = datI != {28'h0000000, 3'b000} ? {sign, sign ? datO[30:0] : datI[30:0]} : 32'h00000000;
-
-endmodule
-
-module ModC2T(
-    input  wire [31:0] C,
-    output wire [31:0] T
-);
-
-    wire        sign; 
-    wire [30:0] datI;
-    assign {sign, datI} = C;
-
-    wire [31:0] datO;
-
-    Add32 adder(
-        .op1({1'b0, datI}), 
-        .op2(32'hFFFFFFFF), 
-        .sum(datO)
-    );  /* ~(datI + -1) 
-           ~ at next line */
-
-    assign T = datI != {28'h0000000, 3'b000} ? {sign, sign ? ~datO[30:0] : datI[30:0]} : 32'h00000000;
 
 endmodule
---
+++ 0x0010_Sub32/TC_converter.v
@@ -0,0 +1,42 @@
+module ModT2C(
+    input  wire [31:0] T,
+    output wire [31:0] C
+);
+
+    wire        sign; 
+    wire [30:0] datI;
+    assign {sign, datI} = T;
+
+    wire [31:0] datO;
+
+    Add32 adder(
+        .op1({1'b0, ~datI}), 
+        .op2(32'h00000001), 
+        .sum(datO)
+    );  /* ~datI + 1 */
+
+    assign C = datI != {28'h0000000, 3'b000} ? {sign, sign ? datO[30:0] : datI[30:0]} : 32'h00000000;
+
+endmodule
+
+module ModC2T(
+    input  wire [31:0] C,
+    output wire [31:0] T
+);
+
+    wire        sign; 
+    wire [30:0] datI;
+    assign {sign, datI} = C;
+
+    wire [31:0] datO;
+
+    Add32 adder(
+        .op1({1'b0, datI}), 
+        .op2(32'hFFFFFFFF), 
+        .sum(datO)
+    );  /* ~(datI + -1) 
+           ~ at next line */
+
+    assign T = datI != {28'h0000000, 3'b000} ? {sign, sign ? ~datO[30:0] : datI[30:0]} : 32'h00000000;
+
+endmodule
