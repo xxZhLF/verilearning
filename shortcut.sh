09b3460e500c8f059b8f126ae94a1ad1b3e187a7
Zhou Xiaoxiao <xxzhou_LF94@outlook.com>
2023-10-15 15:41:27 +0900

    Changing the file folder organization

--- shortcut.sh
+++ shortcut.sh
@@ -6,4 +6,13 @@
 
 echo "RUNNING ----------------------------"
 
+mkdir 0x0021_Mul32U
+touch 0x0021_Mul32U/README.md
+mv 0x0021_MulBT32S 0x0022_MulBT32S 
+rm 0x0022_MulWL32U
+
+git add .
+git commit -m 'Changing the file folder organization'
+git push
+
 echo "------------------------------- DONE"
