dcfe0f513d05dfd95f8d3db9e1c11ec56770e728
Zhou Xiaoxiao <xxzhou_LF94@outlook.com>
2023-10-19 20:11:56 +0900

    64-Bit Carry Save Adder

--- 0x0022_MulBT32S/Add64.v
+++ 0x0022_MulBT32S/Add64.v
@@ -1,9 +1,29 @@
 module Add64(
     input  wire [63:0] op1,
     input  wire [63:0] op2,
+    input  wire [63:0] op3,
     output wire [63:0] sum
 );
 
+    reg  ZERO = 1'b0;
+    reg  NULL;
+    wire cinner;
 
+    AdderCS32bit adderL(
+        .op1(op1[31:0]),
+        .op2(op2[31:0]),
+        .op3(op3[31:0]),
+        .cin(ZERO),
+        .sum(sum[31:0]),
+        .cout(cinner)
+    ); 
+    AdderCS32bit adderH(
+        .op1(op1[63:32]),
+        .op2(op2[63:32]),
+        .op3(op3[63:32]),
+        .cin(cinner),
+        .sum(sum[63:32]),
+        .cout(NULL)
+    );
 
 endmodule
