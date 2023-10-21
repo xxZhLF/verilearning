406a3ae6272c7dad354f47d96352c9860bb494aa
Zhou Xiaoxiao <xxzhou_LF94@outlook.com>
2023-10-21 16:56:52 +0900

    Add Testcase

--- 0x0022_MulBT32S/MulBT32S_tb.v
+++ 0x0022_MulBT32S/MulBT32S_tb.v
@@ -12,7 +12,8 @@ module MulBT32S_tb(
     end
 
     initial begin
-        #5 op1 = 32'd15; op2 = 32'd14940;
+        #5 op1 = 32'd15; op2 =  32'd14940;
+        #5 op1 = 32'd15; op2 = -32'd14940;
         #5 ;
     end
 
