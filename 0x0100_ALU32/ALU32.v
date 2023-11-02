bdafc4de04b723171e1ae56d111e9351dacf43c7
zhou <zhou@axe.bz>
2023-11-02 11:57:12 +0900

    Add Flag port to ALU

--- 0x0100_ALU32/ALU32.v
+++ 0x0100_ALU32/ALU32.v
@@ -7,7 +7,8 @@ module ALU32 (
     input  wire [ 1:0] ctl,
     input  wire [31:0] op1,
     input  wire [31:0] op2,
-    output wire [31:0] res
+    output wire [31:0] res,
+    output wire [ 3:0] flgs
 );
 
     function [31:0] NOT (input [31:0] val);
@@ -22,20 +23,27 @@ module ALU32 (
         return op1 | op2;
     endfunction
 
-    wire        USELESS;
+    wire        cout;
     wire [31:0] sum;
     AdderLC32bit adder(
         .op1(op1),
         .op2(ctl[0] ? NOT(op2) : op2),
         .cin(ctl[0]),
         .sum(sum),
-        .cout(USELESS)
+        .cout(cout)
     );
 
     assign res = `isEQ(ctl, 2'b00) |
                  `isEQ(ctl, 2'b01) ? sum :
                  `isEQ(ctl, 2'b10) ? AND(op1, op2) : 
                  `isEQ(ctl, 2'b11) ?  OR(op1, op2) : 32'hZZZZZZZZ;
+
+    assign flgs = {
+        /* Negative Flag */  sum[31], 
+        /* Zero     Flag */ `isZERO(sum), 
+        /* Carry    Flag */ ~ctl[1] & cout,
+        /* Overflow Flag */ ~ctl[1] & (op1[31] ^ sum[31]) & ~(ctl[1] ^ op1[31] ^ op2[31])
+    };
 
 endmodule
 
--- 0x0100_ALU32/ALU32_tb.v
+++ 0x0100_ALU32/ALU32_tb.v
@@ -7,20 +7,26 @@ module ALU32_tb (
     reg  [ 1:0] ctl;
     reg  [31:0] op1, op2;
     wire [31:0] res;
+    wire [ 3:0] flgs;
 
     initial begin
-           op1 = 32'hFF; op2 = 32'h0F; ctl = 2'b00;
-        #5                             ctl = 2'b01;
-        #5                             ctl = 2'b10;
-        #5                             ctl = 2'b11;
-        #5 ;
+        op1 = 32'h7FFFFFFF; 
+        op2 = 32'h7FFF0000;
+    end
+
+    initial begin
+        ctl = 2'b00; #5;
+        ctl = 2'b01; #5;
+        ctl = 2'b10; #5;
+        ctl = 2'b11; #5;
     end
 
     ALU32 alu(
         .ctl(ctl),
         .op1(op1),
         .op2(op2),
-        .res(res)
+        .res(res),
+        .flgs(flgs)
     );
     
     initial begin
@@ -29,6 +35,7 @@ module ALU32_tb (
         $dumpvars(1, op1);
         $dumpvars(2, op2);
         $dumpvars(3, res);
+        $dumpvars(3, flgs);
     end
 
 endmodule
