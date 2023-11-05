3f4d3e5eb41940ed710f0761303a818536010e31
Zhou Xiaoxiao <xxzhou_LF94@outlook.com>
2023-11-05 23:49:02 +0900

    32-Bit Adder for Float impl

--- 0x0006_Add32F/IEEE754.v
+++ 0x0006_Add32F/IEEE754.v
@@ -2,6 +2,8 @@
 `define IEEE754_V
 
 `include "../IPs_shared/Comparator.v"
+`include "../IPs_shared/MacroFunc.v"
+`include "../0x0010_Sub32T/Sub32.v"
 
 module IEEE754_decompo(
     input  wire [31:0] float,
@@ -41,6 +43,14 @@ module IEEE754_analyzer(
         .op1(exp1),
         .op2(exp2),
         .res(cmp_res)
+    );
+
+    // 无符号减法（追加Sub32U定义）
+    wire [31:0] sub_diff;
+    Sub32 substractor(
+        .op1(`isEQ(cmp_res, `OP1_GT_OP2) ? exp1 : exp2),
+        .op2(`isEQ(cmp_res, `OP1_GT_OP2) ? exp2 : exp1),
+        .diff(sub_diff)
     );
 
 endmodule
