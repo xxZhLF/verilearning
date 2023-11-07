6fd94b59e9f7f168d099c424d6b943be5e43ae07
zhou <zhou@axe.bz>
2023-11-07 14:21:03 +0900

    32-Bit Unsigned Subtractor impl Completed

---
+++ 0x0012_Sub32U/Sub32.v
@@ -0,0 +1,21 @@
+`ifndef SUB_32BIT_COMPLEMENT_V
+`define SUB_32BIT_COMPLEMENT_V
+
+module Sub32(
+    input  wire [31:0] op1,
+    input  wire [31:0] op2,
+    output wire [31:0] diff
+);
+
+    reg USELESS;
+    AdderLC32bit subtractor(  
+        .op1( op1), 
+        .op2(~op2), 
+        .cin(1'b0),
+        .sum(diff),
+        .cout(USELESS)
+    );
+
+endmodule
+
+`endif 
---
+++ 0x0012_Sub32U/Sub32_tb.v
@@ -0,0 +1,32 @@
+`timescale 1ps/1ps
+
+module Sub32_tb (
+    // None
+);
+
+    reg  [31:0] op1, op2;
+    wire [31:0] diff;
+    initial begin
+        op1 = 32'h00000000;
+        op2 = 32'h00000000;
+    end
+
+    Sub32 subtractor(
+        .op1(op1),
+        .op2(op2),
+        .diff(diff)   
+    );
+
+    initial begin
+        #5 op1 =  32'd100; op2 =  32'd55;
+        #5 ;
+    end
+
+    initial begin
+        $dumpfile("Sub32.vcd");
+        $dumpvars(0, op1);
+        $dumpvars(1, op2);
+        $dumpvars(2, diff);
+    end
+
+endmodule
