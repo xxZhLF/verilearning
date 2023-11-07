09375550786a202d5d7918b334204df8091178e5
zhou <zhou@axe.bz>
2023-11-07 18:21:43 +0900

    32-Bit Floating Adder impl...

--- 0x0006_Add32F/Add32F.v
+++ 0x0006_Add32F/Add32F.v
@@ -3,7 +3,10 @@
 
 `include "../IPs_shared/IEEE754.v"
 `include "../IPs_shared/Shift.v"
+`include "../IPs_shared/Comparator.v"
+`include "../IPs_shared/TC_converter.v"
 `include "../0x0003_AdderLC32bit/AdderLC32bit.v"
+`include "../0x0012_Sub32U/Sub32.v"
 
 module Add32F(
     input  wire [31:0] op1,
@@ -25,26 +28,25 @@ module Add32F(
         .sign(s[1])
     );
 
-    wire [31:0] sft_nbit;
-    wire        sft_frac;
-    IEEE754_analyzer analyzer(
+    wire        sft_tgt;
+    wire [31:0] sft_nbs;
+    IEEE754_exponent_process processor_e(
         .exp1(e[0]),
         .exp2(e[1]),
-        .sft_nbit(sft_nbit),
-        .sft_frac(sft_frac)
+        .sft_tgt(sft_tgt),
+        .sft_nbs(sft_nbs)
     );
 
-    wire [31:0] sft_out;
-    ShiftL32U shift(
-        .n(sft_nbit[7:0]),
-        .in(sft_frac ? f[1] : f[0]),
-        .out(sft_out)
+    IEEE754_fraction_process processor_f(
+        .frac(f),
+        .sft_tgt(sft_tgt),
+        .sft_nbs(sft_nbs)
     );
 
     // reg         USELESS;
     // wire [31:0] f_sum;
     // AdderLC32bit adder(
-    //     .op1(~sft_frac ? f[1] : f[0]),
+    //     .op1(sft_frac ? f[0] : f[1]),
     //     .op2(sft_out),
     //     .cin(1'b0),
     //     .sum(f_sum),
@@ -59,11 +61,11 @@ module Add32F(
 
 endmodule
 
-module IEEE754_analyzer(
+module IEEE754_exponent_process(
     input  wire [31:0] exp1,
     input  wire [31:0] exp2,
-    output wire [31:0] sft_nbit,
-    output wire        sft_frac
+    output wire        sft_tgt,
+    output wire [31:0] sft_nbs
 );
 
     wire [ 1:0] cmp_res;
@@ -76,12 +78,30 @@ module IEEE754_analyzer(
     Sub32 subtractor(
         .op1(`isEQ(cmp_res, `OP1_GT_OP2) ? exp1 : exp2),
         .op2(`isEQ(cmp_res, `OP1_GT_OP2) ? exp2 : exp1),
-        .diff(sft_nbit)
+        .diff(sft_nbs)
     );
 
-    assign sft_frac = `isEQ(cmp_res, `OP1_GT_OP2) ? 1'b1 : 1'b0;
-    /*(exp1 > exp2) => (frac2 << exp1-exp2) => (sft_frac = 1'b1)
-      (exp1 < exp2) => (frac1 << exp2-exp1) => (sft_frac = 1'b0)*/
+    assign sft_tgt = `isEQ(cmp_res, `OP1_GT_OP2) ? 1'b1 : 1'b0;
+    /* (exp1 > exp2) => (frac2 << exp1-exp2) => (sft_frac = 1'b1) 
+       (exp1 < exp2) => (frac1 << exp2-exp1) => (sft_frac = 1'b0) */
+
+endmodule
+
+module IEEE754_fraction_process(
+    input  wire [31:0] frac[1:0],
+    input              sft_tgt,
+    input  wire [31:0] sft_nbs
+);
+
+    wire [31:0] frac1, frac2;
+    assign frac1 = ~sft_tgt ? frac[0] : frac[1];
+    ShiftL32U shift(
+        .n(sft_nbs[7:0]),
+        .in(sft_tgt ? frac[1] : frac[0]),
+        .out(frac2)
+    );
+
+
 
 endmodule
 
--- IPs_shared/IEEE754.v
+++ IPs_shared/IEEE754.v
@@ -1,10 +1,6 @@
 `ifndef IEEE754_V
 `define IEEE754_V
 
-`include "../IPs_shared/Comparator.v"
-`include "../IPs_shared/MacroFunc.v"
-`include "../0x0012_Sub32U/Sub32.v"
-
 module IEEE754_decompo(
     input  wire [31:0] float,
     output wire [31:0] fraction,
@@ -14,8 +10,8 @@ module IEEE754_decompo(
 
     assign sign = float[31];
 
-    assign fraction = {1'b1, float[22: 0],  8'b0};
-    assign exponent = {      24'b0, float[30:23]};
+    assign fraction = { 8'b0, 1'b1, float[22: 0]};
+    assign exponent = {24'b0,       float[30:23]};
 
 endmodule
 
