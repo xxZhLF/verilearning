5c88034bba8e82df523cecd9bbe2dd7d285e27b7
Zhou Xiaoxiao <xxzhou_LF94@outlook.com>
2023-11-08 23:58:08 +0900

    Fix Output Bug in C Simulation

--- 0x0006_Add32F/c_sim.c
+++ 0x0006_Add32F/c_sim.c
@@ -80,7 +80,7 @@
 #define show_calc_add(a, b) do {\
     float c = calc_IEEE754(a, b, '+'); \
     show_float(c, ' '); \
-    printf("= %7.2f + %7.2f (Error of CPU: %.20f)\n", a, b, fabs(c - (a + b))); \
+    printf("= %7.2f + %7.2f (Error to CPU: %.20f)\n", a, b, fabs(c - (a + b))); \
     fprintf(fp, "%08X + %08X = %08X\n", *(unsigned int *)&a, *(unsigned int *)&b, *(unsigned int *)&c); \
 } while(0)
 
