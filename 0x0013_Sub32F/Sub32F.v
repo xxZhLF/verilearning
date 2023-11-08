c2ff2594136e94a99e822f2178421f71f836c87c
zhou <zhou@axe.bz>
2023-11-08 18:01:06 +0900

    Prepare for 32-Bit Floating Subtractor

---
+++ 0x0013_Sub32F/Sub32F.v
@@ -0,0 +1,20 @@
+`ifndef SUB_32BIT_FLOAT_V
+`define SUB_32BIT_FLOAT_V
+
+`include "../0x0006_Add32F/Add32F.v"
+
+module Sub32F(
+    input  wire [31:0] op1,
+    input  wire [31:0] op2,
+    output wire [31:0] diff
+);
+
+    Add32F adder(
+        .op1(op1),
+        .op2({~op2[31], op2[30:0]}),
+        .sum(diff)
+    );
+
+endmodule
+
+`endif 
---
+++ 0x0013_Sub32F/Sub32F_tb.v
@@ -0,0 +1,45 @@
+module Sub32F_tb (
+    // None
+);
+
+    integer fd;
+    reg  [31:0] op1, op2, chk;
+    wire [31:0] diff;
+
+    initial begin
+        fd = $fopen("data.tb", "r");
+        if (fd == 0) begin
+            $display("* WARNING: Test Data is NOT Exist!");
+            $display("* SUGGEST: Run \"make genTestData\" to generate, Please.");
+            $finish;
+        end
+
+        while(!$feof(fd)) begin
+            reg  [31:0] a, b, c;
+            $fscanf(fd, "%h + %h = %h", a, b, c);
+            op1 = a; op2 = b; chk = c; #5;
+            if (c == diff) begin
+                $display("[OK] %h - %H = %h",      op1, op2, diff     );
+            end else begin
+                $display("[NG] %h - %h = %h (%h)", op1, op2, diff, chk);
+            end
+        end
+
+        $fclose(fd);
+        $finish;
+    end
+
+    Sub32F adder(
+        .op1(op1),
+        .op2(op2),
+        .diff(diff)
+    );
+
+    initial begin
+        $dumpfile("Sub32F.vcd");
+        $dumpvars(0, op1);
+        $dumpvars(1, op2);
+        $dumpvars(3, diff);
+    end
+
+endmodule
