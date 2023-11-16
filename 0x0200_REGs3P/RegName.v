c9ca061ea8e7a34dfeeea56ac442a67cc441274c
zhou <zhou@axe.bz>
2023-11-16 11:12:38 +0900

    Add Comment to register name

--- 0x0200_REGs3P/RegName.v
+++ 0x0200_REGs3P/RegName.v
@@ -1,38 +1,38 @@
 `ifndef REGISTER_NAME_V
 `define REGISTER_NAME_V
 
-`define zero 5'h00
-`define ra   5'h01
-`define sp   5'h02
-`define gp   5'h03
-`define tp   5'h04
-`define t0   5'h05
-`define t1   5'h06
-`define t2   5'h07
-`define fp   5'h08
-`define s0   5'h08
-`define s1   5'h09
-`define a0   5'h0A
-`define a1   5'h0B
-`define a2   5'h0C
-`define a3   5'h0D
-`define a4   5'h0E
-`define a5   5'h0F
-`define a6   5'h10
-`define a7   5'h11
-`define s2   5'h12
-`define s3   5'h13
-`define s4   5'h14
-`define s5   5'h15
-`define s6   5'h16
-`define s7   5'h17
-`define s8   5'h18
-`define s9   5'h19
-`define s10  5'h1A
-`define s11  5'h1B
-`define t3   5'h1C
-`define t4   5'h1D
-`define t5   5'h1E
-`define t6   5'h1F
+`define zero 5'h00  /* Constant Value 0 */ 
+`define ra   5'h01  /* Retuen Address */
+`define sp   5'h02  /* Stack Pointer */
+`define gp   5'h03  /* Global Pointer */
+`define tp   5'h04  /* Thread Pointer */
+`define t0   5'h05  /* Temporary Pointer */
+`define t1   5'h06  /* Temporary Pointer */
+`define t2   5'h07  /* Temporary Pointer */
+`define fp   5'h08  /* Frame Pointer */
+`define s0   5'h08  /* Saved Register */
+`define s1   5'h09  /* Saved Register */
+`define a0   5'h0A  /* Function Argument / Return Vaule */
+`define a1   5'h0B  /* Function Argument / Return Vaule */
+`define a2   5'h0C  /* Function Argument */
+`define a3   5'h0D  /* Function Argument */
+`define a4   5'h0E  /* Function Argument */
+`define a5   5'h0F  /* Function Argument */
+`define a6   5'h10  /* Function Argument */
+`define a7   5'h11  /* Function Argument */
+`define s2   5'h12  /* Saved Register */
+`define s3   5'h13  /* Saved Register */
+`define s4   5'h14  /* Saved Register */
+`define s5   5'h15  /* Saved Register */
+`define s6   5'h16  /* Saved Register */
+`define s7   5'h17  /* Saved Register */
+`define s8   5'h18  /* Saved Register */
+`define s9   5'h19  /* Saved Register */
+`define s10  5'h1A  /* Saved Register */
+`define s11  5'h1B  /* Saved Register */
+`define t3   5'h1C  /* Temporary Register */
+`define t4   5'h1D  /* Temporary Register */
+`define t5   5'h1E  /* Temporary Register */
+`define t6   5'h1F  /* Temporary Register */
 
 `endif 
