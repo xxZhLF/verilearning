06c67897d182b6af9217dfed1bd050ccbe15151f
zhou <zhou@axe.bz>
2023-11-24 18:21:03 +0900

    Prepare C program for decoder

--- 0x0400_Decoder/prog.c
+++ 0x0400_Decoder/prog.c
@@ -1,8 +1,8 @@
+int buff[16];
 int main(int argc, char* argv[]){
-    unsigned int buff[16] = {0};
     for (unsigned int i = 0; i < 16; ++i){
         if (i < 8) {
-            buff[i] = i;
+            buff[i] = i + 7;
         } else {
             buff[i] = buff[i-8] + i;
         }
