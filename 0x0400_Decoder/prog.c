5240b13fb9880b9b6c37d2ec88face6e0b83406d
zhou <zhou@axe.bz>
2023-11-27 17:55:48 +0900

    Update C program

--- 0x0400_Decoder/prog.c
+++ 0x0400_Decoder/prog.c
@@ -1,10 +1,11 @@
 int buff[16];
 int main(int argc, char* argv[]){
+    int bias = -14;
     for (unsigned int i = 0; i < 16; ++i){
         if (i < 8) {
-            buff[i] = i + 7;
+            buff[i] = i + (bias >> 1);
         } else {
-            buff[i] = buff[i-8] + i;
+            buff[i] = (buff[i-8] + i) > 2;
         }
     }
     return 0;
