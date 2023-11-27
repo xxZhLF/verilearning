9ecac17832b1f5b527f2ad24d5cc33a3aef05d42
zhou <zhou@axe.bz>
2023-11-27 17:57:37 +0900

    Decoder impl complete.

--- 0x0400_Decoder/Decoder.v
+++ 0x0400_Decoder/Decoder.v
@@ -11,24 +11,24 @@ module Decoder (
     output wire [ 4:0] rs2,
     output wire [ 4:0] rd,
     output wire [ 9:0] func,
-    output wire [11:0] imms,
-    output wire [19:0] imml
+    output wire [31:0] imm
 );
 
     assign op = instr[6:0];
 
-    assign {rs1, rs2, rd, func, imms, imml} = `isEQ(instr[6:0], `ITYP_R) ? {rt_rs1, rt_rs2, rt_rd, rt_func,  12'b0,  20'b0} :
-                                              `isEQ(instr[6:0], `ITYP_I_SP) |
-                                              `isEQ(instr[6:0], `ITYP_I) ? {it_rs1,   5'b0, it_rd, it_func, it_imm,  20'b0} :
-                                              `isEQ(instr[6:0], `ITYP_S) ? {st_rs1, st_rs2,  5'b0, st_func, st_imm,  20'b0} :
-                                              `isEQ(instr[6:0], `ITYP_B) ? {bt_rs1, bt_rs2,  5'b0, bt_func, bt_imm,  20'b0} :
-                                              `isEQ(instr[6:0], `ITYP_U) ? {  5'b0,   5'b0, ut_rd,   10'b0,  12'b0, ut_imm} :
-                                              `isEQ(instr[6:0], `ITYP_J) ? {  5'b0,   5'b0, jt_rd,   10'b0,  12'b0, jt_imm} : 57'b0;
+    assign {rs1, rs2, rd, func, imm} = `isEQ(instr[6:0], `INSTR_TYP_R) ? {rt_rs1, rt_rs2, rt_rd, rt_func,  32'b0 } :
+                                       `isEQ(instr[6:0], `INSTR_TYP_ILD) |
+                                       `isEQ(instr[6:0], `INSTR_TYP_IJR) |
+                                       `isEQ(instr[6:0], `INSTR_TYP_I) ? {it_rs1,   5'b0, it_rd, it_func, it_imm } :
+                                       `isEQ(instr[6:0], `INSTR_TYP_S) ? {st_rs1, st_rs2,  5'b0, st_func, st_imm } :
+                                       `isEQ(instr[6:0], `INSTR_TYP_B) ? {bt_rs1, bt_rs2,  5'b0, bt_func, bt_imm } :
+                                       `isEQ(instr[6:0], `INSTR_TYP_U) ? {  5'b0,   5'b0, ut_rd,   10'b0, ut_imm } :
+                                       `isEQ(instr[6:0], `INSTR_TYP_J) ? {  5'b0,   5'b0, jt_rd,   10'b0, jt_imm } : 57'b0;
 
     wire [ 4:0] rt_rs1, rt_rs2, rt_rd;
     wire [ 9:0] rt_func;
     RT_decoder rt (
-        .body(instr[31:7]),
+        .instr(instr),
         .rs1(rt_rs1),
         .rs2(rt_rs2),
         .rd(rt_rd),
@@ -37,9 +37,9 @@ module Decoder (
 
     wire [ 4:0] it_rs1, it_rd;
     wire [ 9:0] it_func;
-    wire [11:0] it_imm;
+    wire [31:0] it_imm;
     IT_decoder it (
-        .body(instr[31:7]),
+        .instr(instr),
         .rs1(it_rs1),
         .rd(it_rd),
         .func(it_func),
@@ -48,9 +48,9 @@ module Decoder (
 
     wire [ 4:0] st_rs1, st_rs2;
     wire [ 9:0] st_func;
-    wire [11:0] st_imm;
+    wire [31:0] st_imm;
     ST_decoder st (
-        .body(instr[31:7]),
+        .instr(instr),
         .rs1(st_rs1),
         .rs2(st_rs2),
         .func(st_func),
@@ -59,9 +59,9 @@ module Decoder (
 
     wire [ 4:0] bt_rs1, bt_rs2;
     wire [ 9:0] bt_func;
-    wire [11:0] bt_imm;
+    wire [31:0] bt_imm;
     BT_decoder bt (
-        .body(instr[31:7]),
+        .instr(instr),
         .rs1(bt_rs1),
         .rs2(bt_rs2),
         .func(bt_func),
@@ -69,17 +69,17 @@ module Decoder (
     );
 
     wire [ 4:0] ut_rd;
-    wire [19:0] ut_imm;
+    wire [31:0] ut_imm;
     UT_decoder ut (
-        .body(instr[31:7]),
+        .instr(instr),
         .rd(ut_rd),
         .imm(ut_imm)
     );
 
     wire [ 4:0] jt_rd;
-    wire [19:0] jt_imm;
+    wire [31:0] jt_imm;
     JT_decoder jt (
-        .body(instr[31:7]),
+        .instr(instr),
         .rd(jt_rd),
         .imm(jt_imm)
     );
@@ -87,95 +87,106 @@ module Decoder (
 endmodule
 
 module RT_decoder (
-    input  wire [24:0] body,
+    input  wire [31:0] instr,
     output wire [ 4:0] rs1,
     output wire [ 4:0] rs2,
     output wire [ 4:0] rd,
     output wire [ 9:0] func
 );
 
-    assign func = {body[24:18], 
-                   body[ 7: 5]};
-    assign rs1  =  body[12: 8];
-    assign rs2  =  body[17:13];
-    assign rd   =  body[ 4: 0];
+    assign func = {instr[31:25], 
+                   instr[14:12]};
+    assign rs1  =  instr[19:15];
+    assign rs2  =  instr[24:20];
+    assign rd   =  instr[11: 7];
 
 endmodule
 
 module IT_decoder (
-    input  wire [24:0] body,
+    input  wire [31:0] instr,
     output wire [ 4:0] rs1,
     output wire [ 4:0] rd,
     output wire [ 9:0] func,
-    output wire [11:0] imm
+    output wire [31:0] imm
 );
 
-    assign func = {7'b0, 
-                   body[ 7: 5]};
-    assign rs1  =  body[12: 8];
-    assign rd   =  body[ 4: 0];
-    assign imm  =  body[24:13];
+    assign rs1 = instr[19:15];
+    assign rd  = instr[11: 7];
+    assign {func, imm} = `isEQ(instr[14:12], 3'b001) | 
+                         `isEQ(instr[14:12], 3'b101) ? {
+                            /* func */ {instr[31:25], instr[14:12]},
+                            /* imm  */ {27'b0, instr[24:20]}
+                         } : {
+                            /* func */ { 7'b0, instr[14:12]}, 
+                            /* imm  */ {{20{instr[31]}}, instr[31:20]}
+                         };
 
 endmodule
 
 module ST_decoder (
-    input  wire [24:0] body,
+    input  wire [31:0] instr,
     output wire [ 4:0] rs1,
     output wire [ 4:0] rs2,
     output wire [ 9:0] func,
-    output wire [11:0] imm
+    output wire [31:0] imm
 );
 
     assign func = {7'b0, 
-                   body[ 7: 5]};
-    assign rs1  =  body[12: 8];
-    assign rs2  =  body[17:13];
-    assign imm  = {body[24:18], 
-                   body[ 4: 0]};
+                   instr[14:12]};
+    assign rs1  =  instr[19:15];
+    assign rs2  =  instr[24:20];
+    assign imm  = {{20{instr[31]}},
+                   instr[31:25], 
+                   instr[11: 7]};
 
 endmodule
 
 module BT_decoder (
-    input  wire [24:0] body,
+    input  wire [31:0] instr,
     output wire [ 4:0] rs1,
     output wire [ 4:0] rs2,
     output wire [ 9:0] func,
-    output wire [11:0] imm
+    output wire [31:0] imm
 );
 
     assign func = {7'b0,
-                   body[ 7: 5]};
-    assign rs1  =  body[12: 8];
-    assign rs2  =  body[17:13];
-    assign imm  = {body[24], 
-                   body[ 0], 
-                   body[23:18], 
-                   body[ 4: 1]};
+                   instr[14:12]};
+    assign rs1  =  instr[19:15];
+    assign rs2  =  instr[24:20];
+    assign imm  = {{19{instr[31]}},
+                   instr[31], 
+                   instr[ 7], 
+                   instr[30:25], 
+                   instr[11: 8],
+                    1'b0};
 
 endmodule
 
 module UT_decoder(
-    input  wire [24:0] body,
+    input  wire [31:0] instr,
     output wire [ 4:0] rd,
-    output wire [19:0] imm
+    output wire [31:0] imm
 );
 
-    assign rd   =  body[ 4:0];
-    assign imm  =  body[19:0];
+    assign rd   =  instr[11: 7];
+    assign imm  = {instr[31:12], 
+                   12'h000};
 
 endmodule
 
 module JT_decoder (
-    input  wire [24:0] body,
+    input  wire [31:0] instr,
     output wire [ 4:0] rd,
-    output wire [19:0] imm
+    output wire [31:0] imm
 );
 
-    assign rd   =  body[ 4:0];
-    assign imm  = {body[24],
-                   body[12: 5],
-                   body[13],
-                   body[23:14]};
+    assign rd   =  instr[11: 7];
+    assign imm  = {{11{instr[31]}},
+                   instr[31],
+                   instr[19:12],
+                   instr[20],
+                   instr[30:21],
+                    1'b0};
 
 endmodule
 
--- 0x0400_Decoder/Decoder_tb.v
+++ 0x0400_Decoder/Decoder_tb.v
@@ -10,8 +10,7 @@ module Decoder_tb(
     wire [ 6:0] op;
     wire [ 4:0] rs1, rs2, rd;
     wire [ 9:0] func;
-    wire [11:0] imms;
-    wire [19:0] imml;
+    wire [31:0] imm;
     Decoder decoder(
         .instr(instr),
         .op(op),
@@ -19,8 +18,7 @@ module Decoder_tb(
         .rs2(rs2),
         .rd(rd),
         .func(func),
-        .imms(imms),
-        .imml(imml)
+        .imm(imm)
     );
 
     parameter L = 16;
@@ -64,70 +62,81 @@ module Decoder_tb(
             instr = instruction; /* Hint: Dealy for $write(). Otherwise the current result 
             (try it by deleting #5) will be output to the console in the next cycle */ #5;
             case (op)
-                `ITYP_R: begin
+                `INSTR_TYP_R: begin
                     case (func)
-                        `ITYP_R_FC_ADD:  $write("ADD with ");
-                        `ITYP_R_FC_SUB:  $write("SUB with ");
-                        `ITYP_R_FC_SLL:  $write("SLL with ");
-                        `ITYP_R_FC_SLT:  $write("SLT with ");
-                        `ITYP_R_FC_SLTU: $write("SLTU with ");
-                        `ITYP_R_FC_XOR:  $write("XOR with ");
-                        `ITYP_R_FC_SRL:  $write("SRL with ");
-                        `ITYP_R_FC_SRA:  $write("SRA with ");
-                        `ITYP_R_FC_OR:   $write("OR with ");
-                        `ITYP_R_FC_AND:  $write("AND with ");
-                        default: $write("*[ERROR]@ITYP_R Func = %b", func);
+                        `R_TYP_FC_ADD:  $write("ADD with ");
+                        `R_TYP_FC_SUB:  $write("SUB with ");
+                        `R_TYP_FC_SLL:  $write("SLL with ");
+                        `R_TYP_FC_SLT:  $write("SLT with ");
+                        `R_TYP_FC_SLTU: $write("SLTU with ");
+                        `R_TYP_FC_XOR:  $write("XOR with ");
+                        `R_TYP_FC_SRL:  $write("SRL with ");
+                        `R_TYP_FC_SRA:  $write("SRA with ");
+                        `R_TYP_FC_OR:   $write("OR with ");
+                        `R_TYP_FC_AND:  $write("AND with ");
+                        default: $write("*[ERROR]@INSTR_TYP_R Func = %b", func);
                     endcase
                     $write("rs1=x%-d, rs2=x%-d, rd=x%-d", rs1, rs2, rd);
                 end 
-                `ITYP_I: begin
+                `INSTR_TYP_I: begin
                     case (func)
-                        `ITYP_I_FC_ADDI:  $write("ADDI with ");
-                        `ITYP_I_FC_SLTI:  $write("SLTI with ");
-                        `ITYP_I_FC_SLTIU: $write("SLTIU with ");
-                        `ITYP_I_FC_XORI:  $write("XORI with ");
-                        `ITYP_I_FC_ORI:   $write("ORI with ");
-                        `ITYP_I_FC_ANDI:  $write("ANDI with ");
-                        `ITYP_I_FC_SLLI:  $write("SLLI with ");
-                        `ITYP_I_FC_SRLI:  $write("SRLI with ");
-                        `ITYP_I_FC_SRAI:  $write("SRAI with ");
-                        default: $write("*[ERROR]@ITYP_I Func=%b", func);
+                        `I_TYP_FC_ADDI:  $write("ADDI with ");
+                        `I_TYP_FC_SLTI:  $write("SLTI with ");
+                        `I_TYP_FC_SLTIU: $write("SLTIU with ");
+                        `I_TYP_FC_XORI:  $write("XORI with ");
+                        `I_TYP_FC_ORI:   $write("ORI with ");
+                        `I_TYP_FC_ANDI:  $write("ANDI with ");
+                        `I_TYP_FC_SLLI:  $write("SLLI with ");
+                        `I_TYP_FC_SRLI:  $write("SRLI with ");
+                        `I_TYP_FC_SRAI:  $write("SRAI with ");
+                        default: $write("*[ERROR]@INSTR_TYP_I Func=%b", func);
                     endcase
-                    $write("rs1=x%-d, rd=x%-d, imm=%-d", rs1, rd, imms);
+                    $write("rs1=x%-d, rd=x%-d, imm=%-d", rs1, rd, $signed(imm));
                 end 
-                `ITYP_S: begin
+                `INSTR_TYP_S: begin
                     case (func)
-                        `ITYP_S_FC_SB:    $write("SB with ");
-                        `ITYP_S_FC_SH:    $write("SH with ");
-                        `ITYP_S_FC_SW:    $write("SW with ");
-                        default: $write("*[ERROR]@ITYP_S Func=%b", func);
+                        `S_TYP_FC_SB: $write("SB with ");
+                        `S_TYP_FC_SH: $write("SH with ");
+                        `S_TYP_FC_SW: $write("SW with ");
+                        default: $write("*[ERROR]@INSTR_TYP_S Func=%b", func);
                     endcase
-                    $write("rs1=x%-d, rs2=x%-d, imm=%-d", rs1, rs2, imms);
+                    $write("rs1=x%-d, rs2=x%-d, imm=%-d", rs1, rs2, $signed(imm));
                 end 
-                `ITYP_B: begin
+                `INSTR_TYP_B: begin
                     case (func)
-                        `ITYP_B_FC_BEQ:   $write("BEQ with ");
-                        `ITYP_B_FC_BEN:   $write("BNE with ");
-                        `ITYP_B_FC_BLT:   $write("BLT with ");
-                        `ITYP_B_FC_BGE:   $write("BGE with ");
-                        `ITYP_B_FC_BLTU:  $write("BLTU with ");
-                        `ITYP_B_FC_BGEU:  $write("BGEU with ");
-                        default: $write("*[ERROR]@ITYP_B Func=%b", func);
+                        `B_TYP_FC_BEQ:  $write("BEQ with ");
+                        `B_TYP_FC_BEN:  $write("BNE with ");
+                        `B_TYP_FC_BLT:  $write("BLT with ");
+                        `B_TYP_FC_BGE:  $write("BGE with ");
+                        `B_TYP_FC_BLTU: $write("BLTU with ");
+                        `B_TYP_FC_BGEU: $write("BGEU with ");
+                        default: $write("*[ERROR]@INSTR_TYP_B Func=%b", func);
                     endcase
-                    $write("rs1=x%-d, rs2=x%-d, imm=%-d", rs1, rs2, imms);
+                    $write("rs1=x%-d, rs2=x%-d, imm=%-d", rs1, rs2, $signed(imm));
                 end 
-                `ITYP_U: begin
-                    $write("LUI with imm=%H", imml);
+                `INSTR_TYP_U: begin
+                    $write("LUI with rd=x%-d, imm=0x%05H", rd, $signed(imm[31:12]));
                 end 
-                `ITYP_J: begin
-                    $write("JAL with imm=%H", imml);
+                `INSTR_TYP_J: begin
+                    $write("JAL with rd=x%-d, imm=0x%05H", rd, $signed(imm));
                 end 
-                `ITYP_I_SP: begin
+                `INSTR_TYP_ILD: begin
                     case (func) 
-                        `ITYP_I_SP_FC_JALR: $write("JALR with ");
-                        default: $write("*[ERROR]@ITYP_I_SP Func=%b", func);
+                        `ILD_TYP_FC_LB:  $write("LB with ");
+                        `ILD_TYP_FC_LH:  $write("LH with ");
+                        `ILD_TYP_FC_LW:  $write("LW with ");
+                        `ILD_TYP_FC_LBU: $write("LBU with ");
+                        `ILD_TYP_FC_LHU: $write("LHU with ");
+                        default: $write("*[ERROR]@INSTR_TYP_ILD Func=%b", func);
                     endcase
-                    $write("rs1=x%-d, rd=x%-d, imm=%-d", rs1, rd, imms);
+                    $write("rs1=x%-d, rd=x%-d, imm=%-d", rs1, rd, $signed(imm));
+                end
+                `INSTR_TYP_IJR: begin
+                    case (func) 
+                        `IJR_TYP_FC_JALR: $write("JALR with ");
+                        default: $write("*[ERROR]@INSTR_TYP_IJR Func=%b", func);
+                    endcase
+                    $write("rs1=x%-d, rd=x%-d, imm=%-d", rs1, rd, $signed(imm));
                 end
                 default: begin
                     $write("*[ERROR] Machine Code is %H, op=%b", instr, op);
