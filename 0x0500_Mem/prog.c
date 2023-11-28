e9bbf4c5a8da817fee17c0e6a95709289af346a5
zhou <zhou@axe.bz>
2023-11-28 18:08:05 +0900

    C program Write to Memory

---
+++ 0x0500_Mem/prog.c
@@ -0,0 +1,12 @@
+int buff[16];
+int main(int argc, char* argv[]){
+    int bias = -14;
+    for (unsigned int i = 0; i < 16; ++i){
+        if (i < 8) {
+            buff[i] = i + (bias >> 1);
+        } else {
+            buff[i] = (buff[i-8] + i) > 2;
+        }
+    }
+    return 0;
+}
