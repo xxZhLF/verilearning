4d736f383dd99035b9733be38bb3f9bec9d3400a
Zhou Xiaoxiao <xxzhou_LF94@outlook.com>
2023-11-28 23:32:18 +0900

    Prepare for Single-cycle Microarchitecture

---
+++ 0x0600_MicroarchSC/MicroarchSC.v
@@ -0,0 +1,11 @@
+`ifndef MICROARCHITECTURE_SINGLE_CYCLE_V
+`define MICROARCHITECTURE_SINGLE_CYCLE_V
+
+module MicroarchSC (
+    input wire rst;
+    input wire clk;
+);
+    
+endmodule
+
+`endif 
---
+++ 0x0600_MicroarchSC/MicroarchSC_tb.v
@@ -0,0 +1,7 @@
+`timescale 1ps/1ps 
+
+module MicroarchSC_tb(
+    //None
+);
+
+endmodule
