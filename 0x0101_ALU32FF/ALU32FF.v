dcddc1c3e72e6fa197fd9dbedd99c802296fdd00
Zhou Xiaoxiao <xxzhou_LF94@outlook.com>
2023-12-02 21:15:26 +0900

    Full Function ALU impl completed

--- 0x0101_ALU32FF/ALU32FF.v
+++ 0x0101_ALU32FF/ALU32FF.v
@@ -5,83 +5,49 @@
 `include "../IPs_shared/universal4inc.v"
 
 module ALU32FF (
-    input  wire [23:0] ctl,
+    input  wire [15:0] ctl,
     input  wire [31:0] op1,
     input  wire [31:0] op2,
     output wire [31:0] res
 );
 
-    wire [31:0] res_of_add;
-    Add32 adder(
+    wire        cout;
+    wire [31:0] res_of_add_or_sub;
+    AdderLC32bit adder(
+        .op1(op1),
+        .op2(ctl[1] ? NOT(op2) : op2),
+        .cin(ctl[1]),
+        .sum(res_of_add_or_sub),
+        .cout(cout)
+    );
+
+    wire [63:0] res_of_mul;
+    MulBT32S multipiler(
         .op1(op1),
         .op2(op2),
-        .sum(res_of_add)
+        .res(res_of_mul)
     );
 
-    wire [31:0] res_of_sub;
-    Sub32 subtractor(
-        .op1(op1),
-        .op2(op2),
-        .diff(res_of_sub)
-    );
-
-    wire [63:0]  res_of_mul_s;
-    MulBT32S multipiler_s(
-        .op1(op1),
-        .op2(op2),
-        .res(res_of_mul_s)
-    );
-
-    wire [31:0] op1_T;
-    CTC32 c2t(
-        .C(op1),
-        .T(op1_T)
-    );
-
-    wire [63:0]  res_of_mul_u;
-    MulWL32U multipiler_u(
-        .op1(`isEQ(ctl, `ALU_CTL_MULHSU) ? {1'b0, op1_T[30:0]} : op1),
-        .op2(op2),
-        .res(res_of_mul_u)
-    );
-
-    wire [63:0] res_of_mul_u_T;
-    TCC64 t2c(
-        .T({op1[31], res_of_mul_u[62:0]}),
-        .C(res_of_mul_u_T)
-    );
-
-    wire [31:0]  res_of_div_s_quoti;
-    wire [31:0]  res_of_div_s_remai;
-    Div32S divider_s(
+    wire [31:0]  res_of_div_quoti;
+    wire [31:0]  res_of_div_remai;
+    Div32U divider(
         .dived(op1),
         .divor(op2),
-        .quoti(res_of_div_s_quoti),
-        .remai(res_of_div_s_remai)
-    );
-
-    wire [31:0]  res_of_div_u_quoti;
-    wire [31:0]  res_of_div_u_remai;
-    Div32U divider_u(
-        .dived(op1),
-        .divor(op2),
-        .quoti(res_of_div_u_quoti),
-        .remai(res_of_div_u_remai)
-    );
-
-    wire [ 1:0] res_of_cmp_s;
-    Cmp32S cmp_s(
-        .op1(op1),
-        .op2(op2),
-        .res(res_of_cmp_s)
+        .quoti(res_of_div_quoti),
+        .remai(res_of_div_remai)
     );
 
     wire [ 1:0] res_of_cmp_u;
-    Cmp32S cmp_u(
-        .op1(op1),
-        .op2(op2),
+    Cmp32S comparater(
+        .op1(ctl[7] ? op1 : {1'b0, op1[30:0]}),
+        .op2(ctl[7] ? op2 : {1'b0, op2[30:0]}),
         .res(res_of_cmp_u)
     );
+
+    wire [ 1:0] res_of_cmp_s;
+    assign res_of_cmp_s =  ~(op1[31]  ^   op2[31]) ? res_of_cmp_u :
+                           (~op1[31]) &   op2[31]  ? `OP1_LT_OP2 :
+                             op1[31]  & (~op2[31]) ? `OP1_GT_OP2 : 2'bZZ;
 
     wire [31:0] res_of_shiftL_u;
     ShiftL32U shiftL_u(
@@ -104,36 +70,37 @@ module ALU32FF (
         .out(res_of_shiftR_s)
     );
 
-    function [31:0]  OR (input [31:0] op1, input [31:0] op2);
-        return op1 | op2;
-    endfunction
-
     function [31:0] AND (input [31:0] op1, input [31:0] op2);
         return op1 & op2;
+    endfunction
+
+    function [31:0]  OR (input [31:0] op1, input [31:0] op2);
+        return op1 | op2;
     endfunction
 
     function [31:0] XOR (input [31:0] op1, input [31:0] op2);
         return op1 ^ op2;
     endfunction
 
-    assign res = `isEQ(ctl, `ALU_CTL_ADD)    ? res_of_add : 
-                 `isEQ(ctl, `ALU_CTL_SUB)    ? res_of_sub :
-                 `isEQ(ctl, `ALU_CTL_MUL)    ? res_of_mul_s[31: 0] :
-                 `isEQ(ctl, `ALU_CTL_DIV)    ? res_of_div_s_quoti :
-                 `isEQ(ctl, `ALU_CTL_REM)    ? res_of_div_s_remai : 
-                 `isEQ(ctl, `ALU_CTL_DIVU)   ? res_of_div_u_quoti :
-                 `isEQ(ctl, `ALU_CTL_REMU)   ? res_of_div_u_remai :
-                 `isEQ(ctl, `ALU_CTL_MULH)   ? res_of_mul_s[63:32] :
-                 `isEQ(ctl, `ALU_CTL_MULHU)  ? res_of_mul_u[63:32] :
-                 `isEQ(ctl, `ALU_CTL_MULHSU) ? res_of_mul_u_T[63:32] :
+    function [31:0] NOT (input [31:0] op);
+        return ~op;
+    endfunction
+
+    assign res = `isEQ(ctl, `ALU_CTL_ADD)    ? res_of_add_or_sub : 
+                 `isEQ(ctl, `ALU_CTL_SUB)    ? res_of_add_or_sub :
+                 `isEQ(ctl, `ALU_CTL_MUL)    ? res_of_mul[31: 0] :
+                 `isEQ(ctl, `ALU_CTL_DIV)    ? res_of_div_quoti :
+                 `isEQ(ctl, `ALU_CTL_REM)    ? res_of_div_remai : 
+                 `isEQ(ctl, `ALU_CTL_MULH)   ? res_of_mul[63:32] :
                  `isEQ(ctl, `ALU_CTL_SLT)    ? {31'b0, `isEQ(res_of_cmp_s, `OP1_LT_OP2)} :
                  `isEQ(ctl, `ALU_CTL_SLTU)   ? {31'b0, `isEQ(res_of_cmp_u, `OP1_LT_OP2)} :
                  `isEQ(ctl, `ALU_CTL_SLL)    ? res_of_shiftL_u :
                  `isEQ(ctl, `ALU_CTL_SRL)    ? res_of_shiftR_u :
                  `isEQ(ctl, `ALU_CTL_SRA)    ? res_of_shiftR_s :
-                 `isEQ(ctl, `ALU_CTL_OR)     ?  OR(op1, op2) :
                  `isEQ(ctl, `ALU_CTL_AND)    ? AND(op1, op2) :
-                 `isEQ(ctl, `ALU_CTL_XOR)    ? XOR(op1, op2) : 32'hZZZZZZZZ;
+                 `isEQ(ctl, `ALU_CTL_OR)     ?  OR(op1, op2) :
+                 `isEQ(ctl, `ALU_CTL_XOR)    ? XOR(op1, op2) :
+                 `isEQ(ctl, `ALU_CTL_NOT)    ? NOT(op1) : 32'hZZZZZZZZ;
 
 endmodule
 
--- 0x0101_ALU32FF/ALU32FF_tb.v
+++ 0x0101_ALU32FF/ALU32FF_tb.v
@@ -6,9 +6,9 @@ module ALU32FF_tb (
     // None
 );
 
-    reg  [63:0] tmp [2:0]; 
+    reg  [63:0] tmp; 
 
-    reg  [23:0] ctl;
+    reg  [15:0] ctl;
     reg  [31:0] op1, op2;
     wire [31:0] res;
 
@@ -56,34 +56,79 @@ module ALU32FF_tb (
             $display("[NG] ALU_CTL_REM: 0x%08X %% 0x%08X = 0x%08X (0x%08X)", op1, op2, res, op1 % op2);
         end
 
-        ctl = `ALU_CTL_DIVU; op1 = 32'h0AE02023; op2 = 32'h80000A12; #5;
-        if (res == op1 / op2) begin 
-            $display("[OK] ALU_CTL_DIVU"); 
-        end else begin
-            $display("[NG] ALU_CTL_DIVU: 0x%08X / 0x%08X = 0x%08X (0x%08X)", op1, op2, res, op1 / op2);
-        end
-
-        ctl = `ALU_CTL_REMU; op1 = 32'h0AE02023; op2 = 32'h80000A12; #5;
-        if (res == op1 % op2) begin 
-            $display("[OK] ALU_CTL_REMU"); 
-        end else begin
-            $display("[NG] ALU_CTL_REMU: 0x%08X %% 0x%08X = 0x%08X (0x%08X)", op1, op2, res, op1 % op2);
-        end
-
         ctl = `ALU_CTL_MULH; op1 = 32'h0012300F; op2 = 32'h8CBDA0FC; #5;
-        tmp[0] = {{32{op1[31]}}, op1} * {{32{op2[31]}}, op2}; 
-        if (res == tmp[0][63:32]) begin 
+        tmp = {{32{op1[31]}}, op1} * {{32{op2[31]}}, op2}; 
+        if (res == tmp[63:32]) begin 
             $display("[OK] ALU_CTL_MULH"); 
         end else begin
-            $display("[NG] ALU_CTL_MULH: 0x%08X * 0x%08X = 0x%08X (0x%016X)", op1, op2, res, tmp[0][63:32]);
+            $display("[NG] ALU_CTL_MULH: 0x%08X * 0x%08X = 0x%08X (0x%016X)", op1, op2, res, tmp[63:32]);
         end
 
-        ctl = `ALU_CTL_MULHU; op1 = 32'h8CBDA0FC; op2 = 32'h0712300F; #5;
-        tmp[1] = {32'b0, op1} * {32'b0, op2}; 
-        if (res == tmp[1][63:32]) begin 
-            $display("[OK] ALU_CTL_MULHU");
+        ctl = `ALU_CTL_SLT; op1 = 32'h0012300F; op2 = 32'h8CBDA0FC; #5;
+        if (res == {31'b0, op1 < op2}) begin 
+            $display("[OK] ALU_CTL_SLT"); 
         end else begin
-            $display("[NG] ALU_CTL_MULHU: 0x%08X * 0x%08X = 0x%08X (0x%016X)", op1, op2, res, tmp[1][63:32]);
+            $display("[NG] ALU_CTL_SLT: 0x%08X < 0x%08X is %s", op1, op2, res[0] ? "True" : "False");
+        end
+
+        ctl = `ALU_CTL_SLTU; op1 = 32'h0012300F; op2 = 32'h8CBDA0FC; #5;
+        if (res == {31'b0, op1 < op2}) begin 
+            $display("[OK] ALU_CTL_SLTU"); 
+        end else begin
+            $display("[NG] ALU_CTL_SLTU: 0x%08X < 0x%08X is %s", op1, op2, res[0] ? "True" : "False");
+        end
+
+        ctl = `ALU_CTL_SLL; op1 = 32'h0012300F; op2 = 32'h00000007; #5;
+        if (res == op1 << op2) begin 
+            $display("[OK] ALU_CTL_SLL"); 
+        end else begin
+            $display("[NG] ALU_CTL_SLL: 0x%08X << %08X = 0x%08X (0x%08X)", op1, op2, res, op1 << op2);
+        end
+
+        ctl = `ALU_CTL_SRL; op1 = 32'h0012300F; op2 = 32'h00000007; #5;
+        if (res == op1 >> op2) begin 
+            $display("[OK] ALU_CTL_SRL"); 
+        end else begin
+            $display("[NG] ALU_CTL_SRL: 0x%08X >> %08X = 0x%08X (0x%08X)", op1, op2, res, op1 >> op2);
+        end
+
+        ctl = `ALU_CTL_SRA; op1 = 32'h8012300F; op2 = 32'h00000007; #5;
+        for (integer i = 0; i < op2; ++i) begin
+            op1 = op1 >> 1;
+            op1[31] = op1[30];
+        end
+        if (res == op1) begin 
+            $display("[OK] ALU_CTL_SRA"); 
+        end else begin
+            $display("[NG] ALU_CTL_SRA: 0x%08X >> %08X = 0x%08X (0x%08X)", op1, op2, res, op1 >> op2);
+        end
+
+        ctl = `ALU_CTL_AND; op1 = 32'h0012300F; op2 = 32'h8CBDA0FC; #5;
+        if (res == {op1 & op2}) begin 
+            $display("[OK] ALU_CTL_AND"); 
+        end else begin
+            $display("[NG] ALU_CTL_AND: 0x%08X & %08X = 0x%08X (0x%08X)", op1, op2, res, op1 & op2);
+        end
+
+        ctl = `ALU_CTL_OR; op1 = 32'h0012300F; op2 = 32'h8CBDA0FC; #5;
+        if (res == {op1 | op2}) begin 
+            $display("[OK] ALU_CTL_OR"); 
+        end else begin
+            $display("[NG] ALU_CTL_OR: 0x%08X | %08X = 0x%08X (0x%08X)", op1, op2, res, op1 | op2);
+        end
+
+        ctl = `ALU_CTL_XOR; op1 = 32'h0012300F; op2 = 32'h8CBDA0FC; #5;
+        if (res == {op1 ^ op2}) begin 
+            $display("[OK] ALU_CTL_XOR"); 
+        end else begin
+            $display("[NG] ALU_CTL_XOR: 0x%08X ^ %08X = 0x%08X (0x%08X)", op1, op2, res, op1 ^ op2);
+        end
+
+        ctl = `ALU_CTL_NOT; op1 = 32'h0012300F; op2 = 32'hZZZZZZZZ; #5;
+        if (res == ~op1) begin 
+            $display("[OK] ALU_CTL_NOT"); 
+        end else begin
+            $display("[NG] ALU_CTL_NOT: ~0x%08X = 0x%08X (0x%08X)", op1, res, ~op1);
         end
 
     end
