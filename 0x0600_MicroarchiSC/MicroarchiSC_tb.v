259909c90efa685464a8d49499ad8f0d62e77336
Zhou Xiaoxiao <xxzhou_LF94@outlook.com>
2023-12-06 23:55:51 +0900

    Update Testbench, divide clock, clock of memory is 2 times of cpu

--- 0x0600_MicroarchiSC/MicroarchiSC_tb.v
+++ 0x0600_MicroarchiSC/MicroarchiSC_tb.v
@@ -6,45 +6,8 @@ module MicroarchiSC_tb(
     // None
 );
 
-    reg rst, clk;
-    initial begin
-        {rst, clk} = 2'b10; 
-    end
-
-    reg                 D_EnWR;
-    reg  [ 1:0]         D_Size;
-    reg  [31:0] I_ABus, D_ABus;
-    reg  [31:0]         D_DBusW;
-    wire [31:0] I_DBus, D_DBusR;
-    MicroarchiSC core(
-        .rst(rst),
-        .clk(clk),
-        .instr(I_DBus),
-        .dataI(D_DBusR),
-        .dataO(D_DBusW),
-        .store_or_load(D_EnWR),
-        .width_of_data(D_Size),
-        .locat_of_data(D_ABus),
-        .where_is_instr(I_ABus)
-    );
-
-    reg         A_EnWR,  B_EnWR;
-    reg  [ 1:0]          B_Size;
-    reg  [31:0] A_ABus,  B_ABus;
-    reg  [31:0] A_DBusW, B_DBusW;
-    wire [31:0] A_DBusR, B_DBusR;
-    Mem4K memory (
-        .clk(clk),
-        .A_EnWR(A_EnWR),
-        .A_ABus(A_ABus),
-        .A_DBusW(A_DBusW),
-        .A_DBusR(A_DBusR),
-        .B_EnWR(B_EnWR),
-        .B_Size(B_Size),
-        .B_ABus(B_ABus),
-        .B_DBusW(B_DBusW),
-        .B_DBusR(B_DBusR)
-    );
+    reg rst;
+    initial rst = 1'b1; 
 
     // initial begin 
     //     integer fd = $fopen("prog.mc", "r");
@@ -76,16 +39,71 @@ module MicroarchiSC_tb(
         #10 rst = 1'b0;
     end 
 
-    reg [31:0] cnt;
-    always #10 clk = ~clk;
-    always @(negedge rst or posedge clk) begin
+    reg clkMEM;
+    initial clkMEM = 1'b0;
+    always #10 clkMEM = ~clkMEM;
+
+    reg         A_EnWR,  B_EnWR;
+    reg  [ 1:0]          B_Size;
+    reg  [31:0] A_ABus,  B_ABus;
+    reg  [31:0] A_DBusW, B_DBusW;
+    wire [31:0] A_DBusR, B_DBusR;
+    Mem4K memory (
+        .clk(clkMEM),
+        .A_EnWR(A_EnWR),
+        .A_ABus(A_ABus),
+        .A_DBusW(A_DBusW),
+        .A_DBusR(A_DBusR),
+        .B_EnWR(B_EnWR),
+        .B_Size(B_Size),
+        .B_ABus(B_ABus),
+        .B_DBusW(B_DBusW),
+        .B_DBusR(B_DBusR)
+    );
+
+    reg [31:0] cntMEM;
+    always @(negedge rst or posedge clkMEM) begin
         if (rst) begin
-            cnt <= 32'b0;
+            cntMEM <= 32'b0;
         end else begin
-            if (cnt == 32'b1 << 9) begin
+            if (cntMEM == 32'b1 << 9) begin
                 $finish;
             end else begin
-                cnt <= cnt + 1;
+                cntMEM <= cntMEM + 1;
+            end
+        end
+    end
+
+    reg clkCPU;
+    initial clkCPU = 1'b0;
+    always @(posedge clkMEM) clkCPU <= ~clkCPU;
+
+    reg                 D_EnWR;
+    reg  [ 1:0]         D_Size;
+    reg  [31:0] I_ABus, D_ABus;
+    reg  [31:0]         D_DBusW;
+    wire [31:0] I_DBus, D_DBusR;
+    MicroarchiSC core(
+        .rst(rst),
+        .clk(clkCPU),
+        .instr(I_DBus),
+        .dataI(D_DBusR),
+        .dataO(D_DBusW),
+        .store_or_load(D_EnWR),
+        .width_of_data(D_Size),
+        .locat_of_data(D_ABus),
+        .where_is_instr(I_ABus)
+    );
+
+    reg [31:0] cntCPU;
+    always @(negedge rst or posedge clkCPU) begin
+        if (rst) begin
+            cntCPU <= 32'b0;
+        end else begin
+            if (cntCPU == 32'b1 << 9) begin
+                $finish;
+            end else begin
+                cntCPU <= cntCPU + 1;
             end
         end
     end
@@ -93,7 +111,10 @@ module MicroarchiSC_tb(
     initial begin
         $dumpfile("MicroarchiSC.vcd");
         $dumpvars(0, rst);
-        $dumpvars(1, clk);
+        $dumpvars(1, clkMEM);
+        $dumpvars(2, cntMEM);
+        $dumpvars(3, clkCPU);
+        $dumpvars(4, cntCPU);
     end
 
 endmodule
