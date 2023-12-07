57d997f07f443a873f4abc73b838e94ef805bddf
zhou <zhou@axe.bz>
2023-12-07 17:48:12 +0900

    Fix Memory annd Core timing

--- 0x0600_MicroarchiSC/MicroarchiSC_tb.v
+++ 0x0600_MicroarchiSC/MicroarchiSC_tb.v
@@ -9,19 +9,6 @@ module MicroarchiSC_tb(
     reg rst;
     initial rst = 1'b1; 
 
-    // initial begin 
-    //     integer fd = $fopen("prog.mc", "r");
-    //     if (fd == 0) begin
-    //         $display("* WARNING: Test Program is NOT Exist!");
-    //         $display("* SUGGEST: Run \"make prog.mc\" to generate, Please.");
-    //         $finish;
-    //     end 
-    //     for (integer i = 2048; !$feof(fd); i += 4) begin
-    //         reg [16*8-1 : 0] mnemonic_p1, mnemonic_p2; 
-    //         addr = i; $fscanf(fd, "%h \t %s \t %s \n", data, mnemonic_p1, mnemonic_p2); #20;
-    //     end #20 rst = 1'b0;
-    // end 
-
     reg [31:0] instr[1023:0];
     initial begin 
         integer  i = 0;
@@ -35,13 +22,13 @@ module MicroarchiSC_tb(
             reg [16*8-1 : 0] mnemonic_p1, mnemonic_p2; 
             $fscanf(fd, "%h \t %s \t %s \n", instr[i], mnemonic_p1, mnemonic_p2);
             A_EnWR = `MM_ENB_W; A_ABus = i; A_DBusW = instr[i]; #20;
-        end A_EnWR = `MM_ENB_W; A_ABus = i; A_DBusW = 32'hFFFF; #20;
-        #10 rst = 1'b0;
+        end A_EnWR = `MM_ENB_W; A_ABus = i; A_DBusW = {16'hFFFF, 16'h0000};
+        #20 rst = 1'b0;
     end 
 
-    reg clkMEM;
-    initial clkMEM = 1'b0;
-    always #10 clkMEM = ~clkMEM;
+    reg clk_base;
+    initial clk_base = 1'b0;
+    always #10 clk_base = ~clk_base;
 
     reg         A_EnWR,  B_EnWR;
     reg  [ 1:0]          B_Size;
@@ -49,7 +36,7 @@ module MicroarchiSC_tb(
     reg  [31:0] A_DBusW, B_DBusW;
     wire [31:0] A_DBusR, B_DBusR;
     Mem4K memory (
-        .clk(clkMEM),
+        .clk(clk_base),
         .A_EnWR(A_EnWR),
         .A_ABus(A_ABus),
         .A_DBusW(A_DBusW),
@@ -61,31 +48,19 @@ module MicroarchiSC_tb(
         .B_DBusR(B_DBusR)
     );
 
-    reg [31:0] cntMEM;
-    always @(negedge rst or posedge clkMEM) begin
-        if (rst) begin
-            cntMEM <= 32'b0;
-        end else begin
-            if (cntMEM == 32'b1 << 9) begin
-                $finish;
-            end else begin
-                cntMEM <= cntMEM + 1;
-            end
-        end
-    end
-
-    reg clkCPU;
-    initial clkCPU = 1'b0;
-    always @(posedge clkMEM) clkCPU <= ~clkCPU;
+    reg clk_core;
+    initial clk_core = 1'b0;
+    always @(posedge clk_base) 
+        clk_core <= ~clk_core;
 
     reg                 D_EnWR;
     reg  [ 1:0]         D_Size;
     reg  [31:0] I_ABus, D_ABus;
     reg  [31:0]         D_DBusW;
-    wire [31:0] I_DBus, D_DBusR;
+    reg  [31:0] I_DBus, D_DBusR;
     MicroarchiSC core(
         .rst(rst),
-        .clk(clkCPU),
+        .clk(clk_core),
         .instr(I_DBus),
         .dataI(D_DBusR),
         .dataO(D_DBusW),
@@ -95,15 +70,52 @@ module MicroarchiSC_tb(
         .where_is_instr(I_ABus)
     );
 
-    reg [31:0] cntCPU;
-    always @(negedge rst or posedge clkCPU) begin
+    reg is_Loading;
+    reg [31:0] cnt;
+    always @(posedge clk_base) begin
         if (rst) begin
-            cntCPU <= 32'b0;
+            is_Loading <= 1'b1;
+            cnt <= 32'b0;
         end else begin
-            if (cntCPU == 32'b1 << 9) begin
+            if (is_Loading) begin
+                is_Loading <= 1'b0;
+            end else begin
+                is_Loading <= is_Loading;
+            end
+            cnt <= cnt + 32'b1;
+        end
+    end
+
+    always @(negedge rst or posedge clk_core) begin
+        if (rst) begin
+        end else begin
+            if (cnt == {32'b1 << 9}) begin
                 $finish;
             end else begin
-                cntCPU <= cntCPU + 1;
+                A_EnWR <= `MM_ENB_R;
+                A_ABus <= I_ABus;
+                if (is_Loading) begin
+                end else begin
+                    I_DBus <= A_DBusR;
+                end
+            end
+        end
+    end
+
+    always @(negedge rst or posedge clk_core) begin
+        if (rst) begin
+        end else begin
+            B_EnWR <= `MM_ENB_R;
+            B_Size <= `MW_Word;
+            if (is_Loading) begin
+                B_ABus <= 32'd2048;
+                $display("+++++++ %08H, %08H", B_ABus, B_DBusR);
+            end else begin
+                B_ABus <= B_ABus + 32'd4;
+                $display("------- %08H, %08H [%s]", B_ABus, B_DBusR, B_DBusR == instr[B_ABus] ? "OK" : "NG");
+            end
+            if (B_DBusR == 32'hFFFF0000) begin
+                $finish;
             end
         end
     end
@@ -111,10 +123,9 @@ module MicroarchiSC_tb(
     initial begin
         $dumpfile("MicroarchiSC.vcd");
         $dumpvars(0, rst);
-        $dumpvars(1, clkMEM);
-        $dumpvars(2, cntMEM);
-        $dumpvars(3, clkCPU);
-        $dumpvars(4, cntCPU);
+        $dumpvars(1, clk_base);
+        $dumpvars(2, clk_core);
+        $dumpvars(3, cnt);
     end
 
 endmodule
