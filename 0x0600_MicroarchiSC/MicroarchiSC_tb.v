dd19717770178c1e12b3be191b98d63232bd23bf
zhou <zhou@axe.bz>
2023-12-15 16:03:13 +0900

    Dump Memory to file in Testbench

--- 0x0600_MicroarchiSC/MicroarchiSC_tb.v
+++ 0x0600_MicroarchiSC/MicroarchiSC_tb.v
@@ -59,6 +59,7 @@ module MicroarchiSC_tb(
         .where_is_instr(I_ABus)
     );
 
+    reg        A_EnWRex;
     reg [31:0] A_ABusEX, A_DBusEX;
     initial begin 
         integer f = $fopen("prog.mc", "r");
@@ -66,16 +67,16 @@ module MicroarchiSC_tb(
             $display("* WARNING: Test Program is NOT Exist!");
             $display("* SUGGEST: Run \"make prog.mc\" to generate, Please.");
             $finish;
-        end
+        end A_EnWRex = `MM_ENB_W;
         for (integer i = 2048; !$feof(f); i += 4) begin
             reg [16*8-1 : 0] mnemonic_p1, mnemonic_p2; A_ABusEX = i; $fscanf(
             f, "%h \t %s \t %s \n", A_DBusEX, mnemonic_p1, mnemonic_p2); #20;
-        end $fclose(f);
-        #30 rst = 1'b0;
+        end $fclose(f); 
+        #30 rst = 1'b0; A_EnWRex = `MM_ENB_R;
     end 
 
-    assign A_EnWR = rst ? `MM_ENB_W : `MM_ENB_R;
-    assign A_ABus = rst ?  A_ABusEX :  I_ABus;
+    assign A_EnWR = A_EnWRex;
+    assign A_ABus = rst ? A_ABusEX : I_ABus;
     assign A_DBusWI = A_DBusEX;
     assign I_DBus   = A_DBusRO;
 
@@ -85,12 +86,25 @@ module MicroarchiSC_tb(
     assign B_DBusWI = D_DBusWO;
     assign D_DBusRI = B_DBusRO;
 
+    reg isFinished; integer f;
+    initial isFinished = 1'b0;
     always @(posedge clk_core) begin
-        if (rst) begin
+        if (rst & ~isFinished) begin
         end else begin
             if ((cnt > {32'b1 << 32'd12})
-                || (I_ABus == 32'b00000)) begin
-                $finish;
+                || (I_ABus == {32{1'b0}})) begin
+                {rst, cnt} <= {1'b1, 32'd4};
+                f = $fopen("RAM.dump", "w");
+                A_ABusEX <= 32'b00;
+                isFinished <= 1'b1;
+            end else if (isFinished) begin
+                if (cnt < 32'd04096) begin
+                    A_ABusEX <= cnt;
+                    $fdisplay(f, "@[%08H] %08H %s", A_ABus, A_DBusRO, A_DBusRO != 32'b0 ? "*" : " ");
+                end else begin
+                    $fdisplay(f, "@[%08H] %08H %s", A_ABus, A_DBusRO, A_DBusRO != 32'b0 ? "*" : " ");
+                    $finish;
+                end cnt <= cnt + 32'd4;
             end else begin
             end
         end
