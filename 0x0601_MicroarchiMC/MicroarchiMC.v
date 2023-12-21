dd87bfe24f093a7da897be57be6c60da28775837
zhou <zhou@axe.bz>
2023-12-21 16:35:22 +0900

    Multi-cycle Microarchitecture impl ...

--- 0x0601_MicroarchiMC/MicroarchiMC.v
+++ 0x0601_MicroarchiMC/MicroarchiMC.v
@@ -19,6 +19,8 @@
 `define DATA_ST `MM_ENB_W
 `define DATA_LD `MM_ENB_R
 
+`define START_POINT_at(sp) (sp - 32'd4)
+
 module MicroarchiMC (
     input  wire        rst,
     input  wire        clk,
@@ -32,9 +34,9 @@ module MicroarchiMC (
     output wire [31:0] where_is_instr
 );
 
-    reg  [ 1:0] pc_mode;
-    reg  [31:0] pc_offset;
-    reg  [31:0] pc_target;
+    /* reg */ wire  [ 1:0] pc_mode;
+    /* reg */ wire  [31:0] pc_offset;
+    /* reg */ wire  [31:0] pc_target;
     wire [31:0] pc_addr;
     wire [31:0] pc_addr_nxt;
     PC pc(
@@ -47,10 +49,10 @@ module MicroarchiMC (
         .addr_ret(pc_addr_nxt)
     );
 
-    reg         rf_en4w;
-    reg  [ 4:0] rf_wA;
-    reg  [31:0] rf_wD;
-    reg  [ 4:0] rf_r0A, rf_r1A;
+    /* reg */ wire          rf_en4w;
+    /* reg */ wire   [ 4:0] rf_wA;
+    /* reg */ wire   [31:0] rf_wD;
+    /* reg */ wire   [ 4:0] rf_r0A, rf_r1A;
     wire [31:0] rf_r0D, rf_r1D;
     REGs3P rf(
         .clk(clk),
@@ -63,9 +65,9 @@ module MicroarchiMC (
         .data_o1(rf_r1D)
     );
 
-    reg  [15:0] alu_ctl;
-    reg  [31:0] alu_op1;
-    reg  [31:0] alu_op2;
+    /* reg */ wire   [15:0] alu_ctl;
+    /* reg */ wire   [31:0] alu_op1;
+    /* reg */ wire   [31:0] alu_op2;
     wire [31:0] alu_res;
     ALU32FF alu(
         .ctl(alu_ctl),
@@ -74,7 +76,7 @@ module MicroarchiMC (
         .res(alu_res)
     );
 
-    reg  [31:0] decoder_instr;
+    /* reg */ wire   [31:0] decoder_instr;
     wire [ 6:0] decoder_op;
     wire [ 9:0] decoder_func;
     wire [ 4:0] decoder_rs1;
@@ -111,9 +113,12 @@ module MicroarchiMC (
     wire [31:0] t2c_resT;
     wire [31:0] t2c_resC;
     TCC32 t2c(
-        .T(t2c_resT),
+        .T({c2t_r0DT[31] ^ c2t_r1DT[31], t2c_resT[30:0]}),
         .C(t2c_resC)
     );
+
+    assign decoder_instr  = instr;
+    assign where_is_instr = rst ? 32'd2048 : pc_addr;
 
     assign c2t_r0DC  = rf_r0D;
     assign c2t_r1DC  = rf_r1D;
@@ -121,7 +126,6 @@ module MicroarchiMC (
     assign rf_r0A    = decoder_rs1;
     assign rf_r1A    = decoder_rs2;
     assign rf_wA     = decoder_rd;
-
 
     function [15:0] MUX_of_ALU_ctl;
         input [6:0] op;
@@ -173,7 +177,7 @@ module MicroarchiMC (
             {`INSTR_TYP_I12LD, `I12LD_TYP_FC_LHU}:  return `ALU_CTL_ADD;
             {`INSTR_TYP_I12JR, `I12JR_TYP_FC_JALR}: return `ALU_CTL_ADD;
             {`INSTR_TYP_I20PC, {10{1'b?}}}:         return `ALU_CTL_ADD;
-            default:                                return {16{1'bZ}};
+            default: return {16{1'bZ}};
         endcase
     end
     endfunction
@@ -183,7 +187,7 @@ module MicroarchiMC (
         input [ 9:0] func;
         input [31:0] op1;
         input [31:0] op1T;
-        input [31:0] pc;
+        input [31:0] pcCrt;
     begin
         casez({op, func})
             {`INSTR_TYP_R,     {10{1'b?}}},
@@ -198,8 +202,8 @@ module MicroarchiMC (
             {`INSTR_TYP_R,     `R_TYP_FC_DIV}, 
             {`INSTR_TYP_R,     `R_TYP_FC_REM}: return {1'b0, op1T[30:0]};
             {`INSTR_TYP_J,     {10{1'b?}}},
-            {`INSTR_TYP_I20PC, {10{1'b?}}}:    return pc;
-            default:                           return {32{1'bZ}};
+            {`INSTR_TYP_I20PC, {10{1'b?}}}:    return pcCrt;
+            default: return {32{1'bZ}};
         endcase
     end
     endfunction
@@ -213,15 +217,109 @@ module MicroarchiMC (
         input [31:0] immT;
     begin
         casez({op, func})
+            {`INSTR_TYP_R,     {10{1'b?}}},
+            {`INSTR_TYP_B,     {10{1'b?}}}:     return op2;
+            {`INSTR_TYP_I,     {10{1'b?}}},
+            {`INSTR_TYP_S,     {10{1'b?}}},
+            {`INSTR_TYP_J,     {10{1'b?}}},
+            {`INSTR_TYP_I12LD, {10{1'b?}}},
+            {`INSTR_TYP_I12JR, {10{1'b?}}}:     return imm;
+            {`INSTR_TYP_R,     `R_TYP_FC_SLT},
+            {`INSTR_TYP_B,     `B_TYP_FC_BLT},
+            {`INSTR_TYP_B,     `B_TYP_FC_BGE}:  return op2T;
+            {`INSTR_TYP_R,     `R_TYP_FC_DIV}, 
+            {`INSTR_TYP_R,     `R_TYP_FC_REM}:  return {1'b0, op2T[30:0]};
+            {`INSTR_TYP_I,     `I_TYP_FC_SLTI}: return immT;
+            {`INSTR_TYP_I20PC, {10{1'b?}}}:     return {decoder_imm[19:0], 12'b0};
             default: return {32{1'bZ}};
         endcase
     end
     endfunction
 
-    reg [2:0] stat;
+    function [31:0] MUX_of_RF;
+        input [ 6:0] op;
+        input [ 9:0] func;
+        input [31:0] res;
+        input [31:0] resT;
+        input [31:0] imm;
+        input [31:0] pcNxt;
+        input [31:0] fromMem;
+    begin
+        casez({op, func})
+            {`INSTR_TYP_R,     {10{1'b?}}},
+            {`INSTR_TYP_I,     {10{1'b?}}},
+            {`INSTR_TYP_I20PC, {10{1'b?}}}:    return res;
+            {`INSTR_TYP_R,     `R_TYP_FC_DIV}: return resT;
+            {`INSTR_TYP_U,     {10{1'b?}}}:    return imm;
+            {`INSTR_TYP_J,     {10{1'b?}}},
+            {`INSTR_TYP_I12JR, {10{1'b?}}}:    return pcNxt;
+            {`INSTR_TYP_I12LD, {10{1'b?}}}:    return fromMem;
+            default: return {32{1'bZ}};
+        endcase
+    end
+    endfunction
+
+    function [1:0] MUX_of_PC;
+        input [ 6:0] op;
+        input [ 9:0] func;
+        input [31:0] rs1;
+        input [31:0] rs2;
+        input [31:0] res;
+    begin
+        if (`isEQ(op, `INSTR_TYP_B)) begin
+            case (func)
+                `B_TYP_FC_BEQ:  return `isEQ(rs1, rs2) ? `BRANCH : `NORMAL;   
+                `B_TYP_FC_BEN:  return `isEQ(rs1, rs2) ? `NORMAL : `BRANCH;
+                `B_TYP_FC_BLT,
+                `B_TYP_FC_BLTU: return      alu_res[0] ? `BRANCH : `NORMAL; 
+                `B_TYP_FC_BGE,
+                `B_TYP_FC_BGEU: return      alu_res[0] ? `NORMAL : `BRANCH; 
+                default: return 2'b00;
+            endcase
+        end else if (`isEQ(op, `INSTR_TYP_J) | (
+                     `isEQ(op, `INSTR_TYP_I12JR) & `isEQ(func, `I12JR_TYP_FC_JALR))
+                    ) begin
+            return `UCJUMP;
+        end else begin
+            return `NORMAL;
+        end
+    end
+    endfunction
+
+    assign alu_ctl = MUX_of_ALU_ctl(decoder_op, decoder_func);
+    assign alu_op1 = MUX_of_ALU_op1(decoder_op, decoder_func,
+                                    rf_r0D, c2t_r0DT, pc_addr);
+    assign alu_op2 = MUX_of_ALU_op2(decoder_op, decoder_func,
+                                    rf_r1D, c2t_r1DT, decoder_imm, c2t_immT);
+
+    assign rf_en4w = `isEQ(decoder_op, `INSTR_TYP_R) |
+                     `isEQ(decoder_op, `INSTR_TYP_I) |
+                     `isEQ(decoder_op, `INSTR_TYP_U) |
+                     `isEQ(decoder_op, `INSTR_TYP_J) |
+                     `isEQ(decoder_op, `INSTR_TYP_I12LD) |
+                     `isEQ(decoder_op, `INSTR_TYP_I12JR) |
+                     `isEQ(decoder_op, `INSTR_TYP_I20PC) ? 1'b1 : 1'b0;
+    assign rf_wD = MUX_of_RF(decoder_op, decoder_func,
+                             alu_res, t2c_resC, decoder_imm, pc_addr_nxt, dataI);
+
+    assign store_or_load = `isEQ(decoder_op,   `INSTR_TYP_S) ? `DATA_ST : `DATA_LD;
+    assign width_of_data = `isEQ(decoder_func, `S_TYP_FC_SB) ? `MW_Byte : 
+                           `isEQ(decoder_func, `S_TYP_FC_SH) ? `MW_Half :
+                           `isEQ(decoder_func, `S_TYP_FC_SW) ? `MW_Word : 2'b00;
+    assign locat_of_data = alu_res;
+    assign dataO = rf_r1D;
+
+    assign pc_mode   = rst ? `UCJUMP : MUX_of_PC(decoder_op, decoder_func,
+                                                 rf_r0D, rf_r1D, alu_res);
+    assign pc_target = rst ? goto : alu_res;
+    assign pc_offset = decoder_imm;
+
+    reg [ 2:0] stat;
+    reg [31:0] goto;
     always @(negedge rst or posedge clk) begin
         if (rst) begin
             stat <= `STAT_HALT;
+            goto <= `START_POINT_at(32'd2048);
         end else begin
             case (stat)
                 `STAT_HALT: stat <= `STAT_IF;
@@ -242,6 +340,14 @@ module MicroarchiMC (
     always @(posedge clk) begin
         if (rst) begin
         end else begin
+            DEBUG_detail_of_instr_exec(instr,
+                                       pc_addr,
+                                       decoder_op,
+                                       decoder_func,
+                                       decoder_imm,
+                                       rf_r0A, rf_r0D,
+                                       rf_r1A, rf_r1D,
+                                       rf_wA,  rf_wD);
         end
     end
 
